1937c7b82da0e72e09d9c0aab88cd96621a0ad85
jmjar <jmjaramillo95@gmail.com>
2025-12-14 20:50:00 -0500

    class-vehiculo

---
+++ fundamentos/src/Vehiculo.java
@@ -0,0 +1,27 @@
+public class Vehiculo {
+
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String color;
+    private double tarifa;
+    private boolean disponible;
+
+    public Vehiculo(String matricula,
+                    String marca,
+                    String modelo,
+                    String color,
+                    double tarifa,
+                    boolean disponible)
+    {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.color = color;
+        this.tarifa = tarifa;
+        this.disponible = disponible;
+    }
+
+
+
+}
